96b32267aafa4bb4cf48af4ba385b05ef93aabfc
litpu <litput0@gmail.com>
2024-04-04 17:32:15 +0300

    Создание проекта

---
+++ Entities/Color.cs
@@ -0,0 +1,6 @@
+﻿namespace Cars.Entities;
+
+public class Color
+{
+    
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddNpgsql<DatabaseContext>(
+    connectionString: "Host=localhost;Port=5432;Username=postgres;Password=root;Database=cars-db");
+builder.Services.AddScoped<>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
