0a4998064feb4c2790d4c6091190444d67d6f5e9
litpu <litput0@gmail.com>
2024-06-16 14:49:24 +0300

    Startup.cs добавлен

--- Cars.API/Program.cs
+++ Cars.API/Program.cs
@@ -1,102 +1,14 @@
-using System.Text;
-using Cars.API.Utils;
-using Cars.Database.Database;
-using Cars.Domain.Mapping;
-using Cars.Domain.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-
+using Cars.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "^_^",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
-
-    
-
-builder.Services.AddAutoMapper(typeof(CarMappingProfile), typeof(EngineMappingProfile), typeof(ManufacturerMappingProfile));
-
-
-builder.Services.AddNpgsql<DatabaseContext>(
-    connectionString: "Host=localhost;Port=5433;Username=postgres;Password=root;Database=cars-db");
-builder.Services.AddScoped<CarService>();
-builder.Services.AddScoped<EngineService>();
-builder.Services.AddScoped<ColorService>();
-builder.Services.AddScoped<CountryService>();
-builder.Services.AddScoped<ManufacturerService>();
-builder.Services.AddScoped<UserService>();
-
-Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-Encoding.RegisterProvider(new CustomEncodingProvider());
-
-var key = Encoding.ASCII.GetBytes("47A486F2-61B1-4C24-B0F8-F5880A8A6C24");
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = "Server",
-        ValidAudience = "Cars",
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ClockSkew = TimeSpan.Zero
-    };
-});
-builder.Services.AddAuthorization();
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
+startup.Configure(app, builder.Environment);
 
 app.Run();
---
+++ Cars.API/Startup.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+using Cars.API.Utils;
+using Cars.Database.Database;
+using Cars.Domain.Mapping;
+using Cars.Domain.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace Cars.API;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "^_^",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
+        services.AddAutoMapper(typeof(CarMappingProfile), typeof(EngineMappingProfile), typeof(ManufacturerMappingProfile));
+
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<DatabaseContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        services.AddScoped<CarService>();
+        services.AddScoped<EngineService>();
+        services.AddScoped<ColorService>();
+        services.AddScoped<CountryService>();
+        services.AddScoped<ManufacturerService>();
+        services.AddScoped<UserService>();
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Encoding.RegisterProvider(new CustomEncodingProvider());
+
+        var jwtKey = Encoding.ASCII.GetBytes(Configuration["Jwt:Key"]);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = Configuration["Jwt:Issuer"],
+                ValidAudience = Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(jwtKey),
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+        services.AddAuthorization();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
