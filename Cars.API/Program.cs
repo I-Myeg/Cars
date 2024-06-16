950692ae063f2fd1f1fa8ce7b3e2506775a476d7
litpu <litput0@gmail.com>
2024-06-16 15:27:25 +0300

    Применение миграций при запуске

--- Cars.API/Program.cs
+++ Cars.API/Program.cs
@@ -1,4 +1,6 @@
 using Cars.API;
+using Cars.Database.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,22 @@ var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
+
+// Apply migrations
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<DatabaseContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception exception)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, "An error occurrred while migrating the database");
+    }
+}
 
 // Configure the HTTP request pipeline.
 startup.Configure(app, builder.Environment);
